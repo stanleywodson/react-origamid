9b28864edddc3d164affc3c8a08dda1b286f1526
Stanley Wodson <stanley.wodson@gmail.com>
2023-11-04 20:43:37 -0300

    it was make install package router and some conf

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
-export const App = () => {
+import { GlobalStyle } from './styles/global'
+import { ThemeProvider } from 'styled-components'
+import { defaultTheme } from './styles/themes/default'
 
+export const App = () => {
   return (
-    <div>
+    <ThemeProvider theme={defaultTheme}>
       <h1>Stanley Wodson</h1>
       <p>More one text in side this place</p>
-    </div>
+      <GlobalStyle />
+    </ThemeProvider>
   )
 }
-
