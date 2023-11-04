7c742b686fb214075c87354d9aa896b8de275429
Stanley Wodson <stanley.wodson@gmail.com>
2023-11-04 20:58:44 -0300

    configured routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import { GlobalStyle } from './styles/global'
 import { ThemeProvider } from 'styled-components'
 import { defaultTheme } from './styles/themes/default'
+import { BrowserRouter } from 'react-router-dom'
+import { AppRoutes } from './components/Router'
 
 export const App = () => {
   return (
-    <ThemeProvider theme={defaultTheme}>
-      <h1>Stanley Wodson</h1>
-      <p>More one text in side this place</p>
-      <GlobalStyle />
-    </ThemeProvider>
+    <BrowserRouter>
+      <ThemeProvider theme={defaultTheme}>
+        <h1>Stanley Wodson</h1>
+        <AppRoutes />
+        <GlobalStyle />
+      </ThemeProvider>
+    </BrowserRouter>
   )
 }
